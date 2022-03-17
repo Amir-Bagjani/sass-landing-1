0fe8714c77b60ac5872903d9f50043b4d49ac6cc
amirbagjni <amirbagjani@gmail.com>
2022-03-17 12:00:14 +0330

    navbar add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
+import "../styles/navbar.scss";
 
 const Navbar = () => {
   return (
     <header className="header">
 
-        
+        <a href="#" className="logo">best <span>App</span></a>
+
+
+        <input type="checkbox" id="menu-btn" />
+        <label htmlFor="menu-btn" className="fas fa-bars"></label>
+
+
+        <nav className="navbar">
+            <a href="#home">home</a>
+            <a href="#features">features</a>
+            <a href="#about">about</a>
+            <a href="#review">review</a>
+            <a href="#pricing">home</a>
+            <a href="#contact">home</a>
+        </nav>
 
     </header>
   )
